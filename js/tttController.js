fde42f1f39acbea16049354fa28f6035350c89e7
coloradojay <delarosa.j@gmail.com>
2014-12-11 19:56:47 -0800

    Hooked up initial variables to Firebase

--- js/tttController.js
+++ js/tttController.js
@@ -6,20 +6,24 @@ function tttCtrlFunc($firebase) {
 
   var self = this;
 
-  var ref = new Firebase("https://thelounge.firebaseio.com/");
-  var sync = $firebase(ref);
+  var ref = new Firebase("https://thelounge.firebaseio.com/game");
+  self.sync = $firebase(ref).$asObject();
 
+  self.sync.name="name";
+  self.sync.turnCount = 0;
+  self.sync.$save();
+  //anything to be saved to firebase needs to have it's name modified to self.sync.<name>
   self.player1 = "";
   self.player2 = "";
   self.gameBoard = ['','','','','','','','',''];
-  self.turnCount = 0;
+
   self.playerTurn = playerTurn;
 
   function playerTurn($index){
-    console.log(self.turnCount);
+    console.log(self.sync.turnCount);
     if (self.gameBoard[$index] === ''){
       // If the game board space is empty, ok to play the game
-      if (self.turnCount % 2 === 0){
+      if (self.sync.turnCount % 2 === 0){
         self.gameBoard[$index] = 'X';
         // track p1's position on the board and save X to the array
       }
@@ -27,7 +31,7 @@ function tttCtrlFunc($firebase) {
         self.gameBoard[$index] = 'O';
         // track p2's position on the board and save X to the array
       }
-        self.turnCount++;
+        self.sync.turnCount++;
         // increase the turnCount by 1
     }
     else { 
