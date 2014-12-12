68430da8b1b64c61ea2933b1633cd128deaf340d
coloradojay <delarosa.j@gmail.com>
2014-12-11 22:47:54 -0800

    Adding in starting game win logic

--- js/tttController.js
+++ js/tttController.js
@@ -11,10 +11,11 @@ function tttCtrlFunc($firebase) {
 
   self.sync.name="name";
   self.sync.turnCount = 0;
+  self.sync.player1 = "";
+  self.sync.player2 = "";
   self.sync.$save();
   //anything to be saved to firebase needs to have it's name modified to self.sync.<name>
-  self.player1 = "";
-  self.player2 = "";
+
   self.gameBoard = ['','','','','','','','',''];
 
   self.playerTurn = playerTurn;
@@ -34,19 +35,50 @@ function tttCtrlFunc($firebase) {
         self.sync.turnCount++;
         // increase the turnCount by 1
     }
-    else { 
-      alert("That space is taken");
-      //If the gameboard space is taken, alert the player that the space is taken
-    }    
+    function gameWin(){
+      if (self.sync.turnCount = 9){
+        alert("Cat's Game");
+      }
+    }
+ 
   }
 
   //game logic
   // if turnCount = 9, game is over and clear the gameBoard array
   // if player has 3 in a row, then alert a message saying he won
-  function gameLogic(){
-    if (turnCount === 9){
-      alert("Cat's Game");
-    }
-  }
+  // function gameLogic(){
+  //   if (turnCount === 9){
+  //     alert("Cat's Game");
+  //   }
+  // }
+      // var isWinCombo = function(cell1, cell2, cell3, player) {
+      //       if (($scope.game.cells[cell1] === player) &&
+      //           ($scope.game.cells[cell2] === player) && 
+      //           ($scope.game.cells[cell3] === player)) {
+      //           return true;
+      //       } else {
+      //           return false;
+      //       }
+      //     };
+      // var winnerIsX = function() {
+      //   return (isWinCombo(0, 1, 2, "X") ||
+      //       isWinCombo(3, 4, 5, "X") || 
+      //       isWinCombo(6, 7, 8, "X") ||
+      //       isWinCombo(0, 3, 6, "X") ||
+      //       isWinCombo(1, 4, 7, "X") || 
+      //       isWinCombo(2, 5, 8, "X") ||
+      //       isWinCombo(0, 4, 8, "X") ||
+      //       isWinCombo(2, 4, 6, "X"));
+      // };
 
+      // var winnerIsO = function() {
+      //   return (isWinCombo(0, 1, 2, "O") ||
+      //       isWinCombo(3, 4, 5, "O") || 
+      //       isWinCombo(6, 7, 8, "O") ||
+      //       isWinCombo(0, 3, 6, "O") ||
+      //       isWinCombo(1, 4, 7, "O") || 
+      //       isWinCombo(2, 5, 8, "O") ||
+      //       isWinCombo(0, 4, 8, "O") ||
+      //       isWinCombo(2, 4, 6, "O"));
+      // };
 }
